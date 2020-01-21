7704a7430c68a64c741f5ba33ce247cf943a5d00
Pridelory <wangmeng.pantera@gmail.com>
2020-01-21 17:13:24 +0800

    second commit

---
+++ myshop-service-reg/src/main/java/com/funtl/myshop/service/reg/MyShopServiceRegApplication.java
@@ -0,0 +1,13 @@
+package com.funtl.myshop.service.reg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MyShopServiceRegApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyShopServiceRegApplication.class, args);
+    }
+}
